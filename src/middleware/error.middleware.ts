8cbf69ac351723a4bb3f15b3883f016307ff5d64
Himal Subedi <itissubedi@gmail.com>
2025-12-27 08:47:05 +0545

    fix: add fail safe to genericErrorHandler

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,9 +3,9 @@ import { NextFunction, Request, Response } from "express"
 import { AppError } from "../utils/errors/app.error"
 
 export const genericErrorHandler = (err:  AppError, req: Request, res: Response, next: NextFunction)=>{
-    res.status(err.statusCode).json({
-        success: "false",
-        message: err.message
-    })
+    res.status(err.statusCode?? 500).json({
+        success: false,
+        message: err.message?? "Something Went Wrong Generic Error Handler caught it"
+    });
 
 }
